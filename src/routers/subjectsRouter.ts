bef2939778c1409405fc8a6cdd70a6fd615edd58
ronald <ronaldjunior2@hotmail.com>
2021-08-09 15:19:35 -0300

    Preparando revisão de código

--- src/routers/subjectsRouter.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router } from "express";
-import * as subjectsController from "../controllers/subjectsController";
-
-const subjectsRouter: Router = express.Router();
-
-subjectsRouter.get("/:id/professors", subjectsController.getSubjectProfessors)
-subjectsRouter.get("", subjectsController.getSubjects);
-
-export default subjectsRouter;
